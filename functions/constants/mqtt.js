68ef0ee547f3200bd15a806fba8c7afd30b4e2dd
Romain Lembo <romainlembo06@gmail.com>
2019-02-15 13:25:57 +0100

    use MQTT Dashboard

--- functions/constants/mqtt.js
+++ functions/constants/mqtt.js
@@ -1,4 +1,8 @@
 module.exports = {
+    broker: {
+        url: "mqtt://broker.hivemq.com",
+        urlMock: "mqtt://192.168.1.25",
+    },
     topics: {
         test: "test",
         temperature: "temperature",
--- functions/mqtt/subscriber.js
+++ functions/mqtt/subscriber.js
@@ -1,5 +1,6 @@
 const mqtt = require('mqtt')
-const client  = mqtt.connect('mqtt://192.168.1.25');
+const { broker: { url } } = require('../constants/mqtt');
+const client  = mqtt.connect(url);
 
 module.exports = {
     subscribe: topic => client.on('connect', () => {
