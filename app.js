976bcd874f789e39f4f0791cf3313c23165c41e3
Romain Lembo <rlembo@vulog.com>
2019-03-06 17:43:18 +0100

    add http server + express

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
-const mqtt = require('mqtt');
-const { 
-    broker: { url },
-    topics: { 
-        temperatures, 
-        brightnesses, 
-        notificationWeb,
-    } 
-} = require('./constants/mqtt');
-const { addTemperature } = require('./models/temperatures.model');
-const { addBrightness } = require('./models/brightnesses.model');
+const mqtt = require("mqtt");
+const express = require("express");
+const http = require("http");
+const {
+  broker: { url },
+  topics: { temperatures, brightnesses, notificationWeb }
+} = require("./constants/mqtt");
+const { addTemperature } = require("./models/temperatures.model");
+const { addBrightness } = require("./models/brightnesses.model");
+
+const app = express();
+const port = process.env.PORT || 8080;
+const server = http.Server(app);
 
 const mqttApi = () => {
+  const client = mqtt.connect(url);
 
-    const client = mqtt.connect(url,);
+  client.on("connect", () => {
+    client.subscribe(temperatures);
+    client.subscribe(brightnesses);
+    console.log("client connected");
+  });
 
-    client.on('connect', () => {
-        client.subscribe(temperatures);
-        client.subscribe(brightnesses);
-        console.log('client connected');
-    });
-  
-    client.on('error', (err) => {
-      console.error(err);
-    });
+  client.on("error", err => {
+    console.error(err);
+  });
 
-    client.on('message', async (topic, message) => {
-        data = message.toString();
-        if(!!data) {
-            if(topic === temperatures) {
-                await addTemperature(JSON.parse(data));
-            }
-            if(topic === brightnesses) {
-                await addBrightness(JSON.parse(data));
-            }
-            await client.publish(notificationWeb, topic);
-        }
-    });
-  
+  client.on("message", async (topic, message) => {
+    data = message.toString();
+    if (!!data) {
+      if (topic === temperatures) {
+        await addTemperature(JSON.parse(data));
+      }
+      if (topic === brightnesses) {
+        await addBrightness(JSON.parse(data));
+      }
+      await client.publish(notificationWeb, topic);
+    }
+  });
 };
 
 mqttApi();
+
+server.listen(port, () => {
+  console.log(`Listening on port:${port}/`);
+});
