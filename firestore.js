014aea33e556cd45559a09b56f065028cedc815a
Romain Lembo <rlembo@vulog.com>
2019-03-06 17:26:18 +0100

    fix account

--- firestore.js
+++ firestore.js
@@ -1,14 +1,9 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceGoogleAccount.json");
 
-const config = {
-    apiKey: "AIzaSyDotoIZIX0t33vT2Tn7jrPKN4NHh1GBioc",
-    authDomain: "tp3-iot-m1-miage.firebaseapp.com",
-    databaseURL: "https://tp3-iot-m1-miage.firebaseio.com",
-    projectId: "tp3-iot-m1-miage",
-    storageBucket: "tp3-iot-m1-miage.appspot.com",
-    messagingSenderId: "556238100610"
-};
-
-admin.initializeApp(config);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tp3-iot-m1-miage.firebaseio.com"
+});
 
 module.exports = admin.firestore();
